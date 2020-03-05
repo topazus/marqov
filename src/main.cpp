169bbedc63d9f7aef6492ad3b3d534440e8a710d
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2020-03-05 22:05:11 +0100

    todays state

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,23 @@
+#include "metropolis.h"
 
-
-class Grid {};
+class Grid {
+    std::vector<int> getnbr(int i);
+};
 
 /** This 
  */
 struct Interaction
 {
     const double J;
-    double operator(StateVector phi_i, StateVector phi_j);
+    double operator(StateVector phi_i, StateVector phi_j)
+    {
+        return (D_x * phi_x, D_y * phi_y ); //diagonal, unequal coupling within the structure of a statevector
+        
+        return 1/abs(i -j);//could also be from a table, e.g Dij[i,j]
+    }
 private:
     double [] Dij;
+    Grid& grid;
 };
 
 struct OnSite
@@ -28,8 +36,6 @@ private:
     double data;
 };
 
-
-
 class Hamiltonian {
     typedef something StateVector;
     const uint Nalpha;
@@ -39,11 +45,29 @@ class Hamiltonian {
     Interaction[Nalpha] interactions;
     OnSite[Nbeta] onsite;
     MultiSite[Ngamma] multisite;
+    
+    StateVector creatersv(StateVector old);
 };
 
-class Marqov {};
-
-void metropolis();
+class Marqov {
+    void elementaryMCstep()
+    {
+        for(int i = 0; i < grid.size(); ++i)
+        {
+            metropolisstep(statespace);
+        }
+    }
+    
+    void gameloop()
+    {
+        for (int i = 0; i < nstep; ++i)
+            elementaryMCstep();
+        
+        for(int j = 0; j < nobs; ++j)
+            obs[j].measure(statespace);//FIXME: consider that there might be reuse across observables!
+    }
+    
+};
 
 void wolff();
 
@@ -53,4 +77,5 @@ class Observable {};
 
 int main()
 {
+    
 }
