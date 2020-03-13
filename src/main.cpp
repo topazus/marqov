9bd9568bb371896ec5859d6d653648cb2f4853a8
Manuel Schrauth <manuel.schrauth@live.de>
2020-03-13 08:17:17 +0100

    added some doc

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include "rndwrapper.h"
 #include "regular_lattice.h"
+#include "vectorhelpers.h"
 
 using std::cout;
 using std::endl;
@@ -71,34 +72,7 @@ StateVector operator - (StateVector lhs,  StateVector rhs)
 template<class VecType>
 inline typename VecType::value_type dot(const VecType& a, const VecType& b)
 {
-        typedef typename VecType::value_type FPType;
-//         //Neumaier summation
-//         FPType sum = a[0]*b[0];
-//         FPType c = 0.0;
-//         for (uint i = 1; i < a.size(); ++i)
-//         {
-//             FPType input = a[i]*b[i];
-//             FPType t = sum + input;
-//             if (std::abs(t) >= std::abs(input))
-//                 c += (sum - t) + input;
-//             else
-//                 c += (input - t) + sum;
-//             sum = t;
-//         }
-//       return sum;
-
-//Kahan summation
-//     FPType sum = 0.0;
-//     FPType c = 0.0;
-//     for (uint i = 0; i < a.size(); ++i)
-//     {
-//         FPType y = a[i]*b[i] - c;
-//         FPType t = sum + y;
-//         c = (t - sum) -y;
-//         sum = t;
-//     }
-//       return sum;
-//plain version
+    typedef typename VecType::value_type FPType;
     return std::inner_product(begin(a), end(a), begin(b), 0.0);
 }
 
@@ -120,14 +94,19 @@ template <class Grid, class Hamiltonian>
 class Marqov 
 {
 	public:
-        typedef typename Hamiltonian::StateVector StateVector;
-	    typedef StateVector* StateSpace;
-	    Marqov(Grid& lattice) : ham(),  grid(lattice), rng(0, 1), metro(rng) {
-                	rng.seed(42);
-                    rng.seed(time(NULL));
-                    rng.set_integer_range(lattice.size());
-	        statespace = new typename Hamiltonian::StateVector[lattice.size()];
-	    }
+		typedef typename Hamiltonian::StateVector StateVector;
+		typedef StateVector* StateSpace;
+
+		// Constructor
+		Marqov(Grid& lattice) : ham(),  grid(lattice), rng(0, 1), metro(rng) 
+		{
+		  	rng.seed(42);
+		  	rng.seed(time(NULL));
+		  	rng.set_integer_range(lattice.size());
+		  	statespace = new typename Hamiltonian::StateVector[lattice.size()];
+		}
+
+		// Definition of an EMCS
 	    void elementaryMCstep()
 	    {
 	        for(int i = 0; i < grid.size(); ++i)
@@ -154,10 +133,12 @@ class Marqov
 			{
 				for(int j = 0; j < grid.length; ++j)
 				{
-					double current = statespace[grid.length*i+j][0];
+					const int dim = 2;
+					double current = statespace[grid.length*i+j][dim];
+
 					if (current > 0) cout << "o ";
 					else if (current < 0) cout << ". ";
-					else cout << "  ";
+					else cout << "x  ";
 				}
 				cout << endl;
 			}
@@ -170,79 +151,97 @@ class Marqov
 				statespace[i][0] = 1;
 			}
 		 }
+
+		 void init_hot()
+		 {
+			for(int i = 0; i < grid.size(); ++i)
+			{
+				statespace[i] = rnddir<RND, double, 3>(rng);
+			}
+		 }
 	
 	private:
-        // Single Metropolis update step statevectors on a lattice
-// returns an integer which encodes whether the flip attempt was successful (1) or not (0)
-inline int metropolisstep(int rsite)
-{
-    StateVector& svold = statespace[rsite];
-    StateVector svnew = metro.newsv(svold);
-    
-    double interactionenergydifference = 0;
-    for(int a = 0; a < ham.Nalpha; ++a)
-    {
-        auto nbrs = grid.getnbrs(a, rsite);
-        StateVector averagevector = {0};
-
-        for (int i = 0; i < nbrs.size(); ++i)
-        {
-            auto mynbr = nbrs[i];
-            auto myvec = ham.interactions[a](statespace[mynbr]);
-            averagevector = averagevector + myvec;
-        }
-        interactionenergydifference += ham.interactions[a].J * (dot(svnew - svold, averagevector));
-    }
-
-    double onsiteenergyold = 0;
-    double onsiteenergynew = 0;
-    for (int b = 0; b < ham.Nbeta; ++b)
-    {
-        onsiteenergyold += dot(ham.onsite[b]->h, ham.onsite[b]->operator()(svold));
-        onsiteenergynew += dot(ham.onsite[b]->h, ham.onsite[b]->operator()(svnew));
-    }
-    
-    double multisiteenergyold = 0;
-    double multisiteenergynew = 0;
-    for (int g = 0; g < ham.Ngamma; ++g)
-    {
-        multisiteenergynew += ham.multisite[g]->operator()(svnew, rsite, statespace);//FIXME: think about this...
-        multisiteenergyold += ham.multisite[g]->operator()(svold, rsite, statespace);//FIXME: think about this...
-        //forgot k_gamma
-    }
-    
-    double dE = interactionenergydifference
-	 + (onsiteenergynew - onsiteenergyold);
-    + (multisiteenergynew - multisiteenergyold);
 
 
-    int retval = 0;
-    if ( dE <= 0 )
-    {
-        svold = svnew;
-        retval = 1;
-    }
-    else if (rng.d() < exp(-ham.beta*dE))
-    {
-        svold = svnew;
-        retval = 1;
-    }
-    
-    return retval;
-}
+	// Single Metropolis update step statevectors on a lattice
+	// returns an integer which encodes whether the flip attempt was successful (1) or not (0)
+	inline int metropolisstep(int rsite)
+	{
+	    StateVector& svold = statespace[rsite];
+	    StateVector svnew = metro.newsv(svold);
+	    
+	    double interactionenergydifference = 0;
+	    for(int a = 0; a < ham.Nalpha; ++a)
+	    {
+	        auto nbrs = grid.getnbrs(a, rsite);
+	        StateVector averagevector = {0};
+	
+	        for (int i = 0; i < nbrs.size(); ++i)
+	        {
+	            auto mynbr = nbrs[i];
+	            auto myvec = ham.interactions[a](statespace[mynbr]);
+	            averagevector = averagevector + myvec;
+	        }
+	        interactionenergydifference += ham.interactions[a].J * (dot(svnew - svold, averagevector));
+	    }
+	
+	    double onsiteenergyold = 0;
+	    double onsiteenergynew = 0;
+	    for (int b = 0; b < ham.Nbeta; ++b)
+	    {
+	        onsiteenergyold += dot(ham.onsite[b]->h, ham.onsite[b]->operator()(svold));
+	        onsiteenergynew += dot(ham.onsite[b]->h, ham.onsite[b]->operator()(svnew));
+	    }
+	    
+	    double multisiteenergyold = 0;
+	    double multisiteenergynew = 0;
+	    for (int g = 0; g < ham.Ngamma; ++g)
+	    {
+	        multisiteenergynew += ham.multisite[g]->operator()(svnew, rsite, statespace);//FIXME: think about this...
+	        multisiteenergyold += ham.multisite[g]->operator()(svold, rsite, statespace);//FIXME: think about this...
+	        //forgot k_gamma
+	    }
+	    
+	    double dE = interactionenergydifference
+		 + (onsiteenergynew - onsiteenergyold);
+	    + (multisiteenergynew - multisiteenergyold);
+	
+	
+	    int retval = 0;
+	    if ( dE <= 0 )
+	    {
+	        svold = svnew;
+	        retval = 1;
+	    }
+	    else if (rng.d() < exp(-ham.beta*dE))
+	    {
+	        svold = svnew;
+	        retval = 1;
+	    }
+	    
+	    return retval;
+	}
 
-        void wolff()
-        {
-        }
-        StateSpace statespace;
-	    Hamiltonian ham;
-	    Grid& grid;
-        RND rng;
-        //Get the MetroInitializer from the user, It's required to have one argument left, the RNG.
-        typename Hamiltonian::template MetroInitializer<RND> metro;//C++11
-	    static constexpr uint nobs = 0;
-	    Observable<StateSpace> obs[5];
-	    static constexpr int nstep = 250;
+
+	void wolff()
+	{
+	}
+
+
+	StateSpace statespace;
+	Hamiltonian ham;
+	Grid& grid;
+	RND rng;
+
+	//Get the MetroInitializer from the user, It's required to have one argument left, the RNG.
+	typename Hamiltonian::template MetroInitializer<RND> metro;//C++11
+
+	// number of observables
+	static constexpr uint nobs = 0;
+	Observable<StateSpace> obs[5];
+
+	// number of EMCS
+	static constexpr int nstep = 250;
 };
 
 int main()
@@ -253,7 +252,7 @@ int main()
     
     Marqov<RegularLattice, Heisenberg<double, double> > marqov(lattice);
 
-	marqov.init_cold();
+	marqov.init_hot();
 	marqov.print_state();
 	marqov.gameloop();
 	cout << endl << endl;
