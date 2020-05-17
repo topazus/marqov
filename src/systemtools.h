a7f5e64556cd1a7c93d0690b8bf7c6f6657e620e
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2020-05-18 00:14:50 +0200

    remove things that according to GCC-10 are redundant

--- src/systemtools.h
+++ src/systemtools.h
@@ -19,7 +19,7 @@ void makeDir(std::string path)
 const std::string currentDateTime() 
 {
 	time_t     now = time(0);
-	struct tm  tstruct;
+	tm  tstruct;
 	char       buf[80];
 	tstruct = *localtime(&now);
 	strftime(buf, sizeof(buf), "%Y-%m-%d %X", &tstruct);
