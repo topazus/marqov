869a7f690962d27fbb41b4663a96c23569f8c58c
Manuel Schrauth <manuel.schrauth@physik.uni-wuerzburg.de>
2020-06-09 00:26:25 +0200

    RegularLattice does now provide coordinates (untested)

--- src/regular_lattice.h
+++ src/regular_lattice.h
@@ -21,42 +21,75 @@ public:
 
     std::vector<double> getcrds(int i) const 
     {
-    // implement me!
-    std::vector<double> retval(3,0);
-    	return retval;
+
+    		// Todo: 
+		// - test me!
+		// - closed formula for the n-d case?
+		// - length of retval is known at compile time -> probably not use vectors
+
+
+    		std::vector<double> retval(dim);
+
+		// transform one-dimensional index to n-d coordinates
+
+		if (dim == 2)
+		{
+			retval[0] = i % length;
+			retval[1] = i / length; // integer division
+		}
+		else if (dim == 3)
+		{
+			int j = i % (length*length);
+
+			retval[0] = i / (length*length);
+			retval[1] = j / length;;
+			retval[2] = j % length;
+		}
+		else
+		{
+			std::cout << "error! not implemented!" << std::endl;
+		}
+
+    		return retval;
 	}
 
-    value_type operator[](int i) const
-    {
-        std::vector<int> temp(2*dim);
-        //calculate neighbours for site i
-        for(int j = 0; j < dim; ++j)
-        {
-            int pl = pows[j]*length;
-            //positive additions
-            int c = i + pows[j];
-            //test positive additions for PBCs
-            if (c >= (c/pl)*pl)
-            {//PBC
-                temp[2*j] = (i/pl)*pl + c % pl;
-            }
-            else
-                temp[2*j] = c;
-            //      std::cout<<i<<" "<<temp[2*j]<<std::endl;
-            
-            //negative additions
-            c = i - pows[j];
-            //test negative additions for PBCs
-            if(c < (i/pl)*pl)
-            {
-                temp[2*j+1] = (i/pl)*pl + (c + pl) % pl;
-            }
-            else
-                temp[2*j+1] = c;
-            //      std::cout<<i<<" "<<temp[2*j+1]<<std::endl;
-        }
-        return temp;
-    }
+		value_type operator[](int i) const
+		{
+			std::vector<int> temp(2*dim);
+			//calculate neighbours for site i
+			for(int j = 0; j < dim; ++j)
+			{
+				int pl = pows[j]*length;
+
+				//positive additions
+				int c = i + pows[j];
+
+				//test positive additions for PBCs
+				if (c >= (c/pl)*pl)
+				{
+					temp[2*j] = (i/pl)*pl + c % pl;
+				}
+				else
+				{
+					temp[2*j] = c;
+				}
+				
+				//negative additions
+				c = i - pows[j];
+
+				//test negative additions for PBCs
+				if (c < (i/pl)*pl)
+				{
+					temp[2*j+1] = (i/pl)*pl + (c + pl) % pl;
+				}
+				else
+				{
+					temp[2*j+1] = c;
+				}
+			}
+			return temp;
+		}
+
     std::size_t size() const {return numberatoms;}
     int length;
     int dim;
