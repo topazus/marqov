4dd14f8f7b300ea6ba7bf0bc752286e6874d1549
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2020-10-29 20:41:55 +0100

    fix compilation

--- src/marqovscheduler.h
+++ src/marqovscheduler.h
@@ -34,41 +34,55 @@ template <class Sim>
 class Scheduler
 {
 private:
-    class Master
-    {
-        bool selectedforpt(Sim sim)
-        {
-            return ptplan[sim.npt].first == sim.id || ptplan[sim.npt].second == sim.id
-        }
-        void operator()()
-        {
-            Simstate itm;
-            while(true)
-            {
-                masterwork.wait([&]{
-                 workqueue.pop_fron(itm);
-                });
-                if(selectedforpt(itm))
-                {
-                    // do pt
-                }
-                else
-                {
-                    taskqueue.enqueue(GameLoop());
-                }
-            }
-        }
-    };
 public:
  void enqueuesim(Sim&& sim) ///< the entry point for the user. Currently it's undecided whether the sim is instantiated by the user or by the scheduler
  {
-     
+     simvector.push_back(sim);//FIXME: Currently I don't know how a sim terminates...
  }
  void start()
  {
-     taskqueue.enqueue(Master());
+     auto master = [&] /*the master thread is a lambda function since by that it captures the variables of the Scheduler*/
+     {
+            Simstate itm;
+            auto gameloop = [&]
+            {
+                std::cout<<"Gamelooping on item"<<std::endl;
+                // work
+                itm.npt = itm.npt + 1;
+                if (itm.npt < maxpt) // determine whether this itm needs more work
+                    workqueue.push_back(itm);
+            };
+            auto warmuploop = [&]
+            {
+                std::cout<<"Warmuplooping on item"<<std::endl;
+                //work here
+                //enqueue the next full work item into the workqueue immediately
+//                workqueue.push_back(gameloop);
+            };
+            while(true)
+            {
+                std::cout<<"Master waiting for work"<<std::endl;
+                bool busy = false;
+                masterwork.wait([&]{
+                 busy = workqueue.pop_front(itm);
+                 return busy;
+                });
+                if(busy) //there really is sth. to do
+                {
+                    std::cout<<"dealing with work"<<std::endl;
+                    if(ptplan[itm.npt].first == itm.id || ptplan[itm.npt].second == itm.id) // check if this sim is selected for PT
+                        ptstep();
+                    else
+                    {
+                    std::cout<<"Putting a new item into the taskqueue"<<std::endl;
+                    taskqueue.enqueue(gameloop);
+                    }
+                }
+            }
+    };
+     taskqueue.enqueue(master);
  }
- void waitforall();
+ void waitforall() {}
  Scheduler(int maxptsteps) : taskqueue(std::thread::hardware_concurrency() + 1),/*a space for the master thread*/
  masterwork{},
  workqueue(masterwork),
@@ -85,17 +99,7 @@ private:
         int id;
         int npt;
     };
-    
-    class GameLoop
-    {
-        void operator()(){}
-    };
-    class WarmUpLoop
-    {
-        void operator()(){}
-    };
-    
-    
+
 MARQOVQueue taskqueue; ///< this is the queue where threads pull their work from
 Semaphore masterwork; ///< the semaphore that triggers the master process
 ThreadSafeQueue<Simstate> workqueue; ///< this is the queue where threads put their finished work and the master does PT
@@ -103,7 +107,7 @@ std::vector<Sim> simvector; ///< An array for the full state of the simulations
 std::vector<int> ptqueue; ///< here we collect who is waiting for its PT partner
 std::vector<std::pair<int, int> > ptplan;///< who exchanges with whom in each step
 int maxpt; ///< how many pt steps do we do
-void ptstep();
+void ptstep() {}
 void calcprob();
 void exchange();
 };
