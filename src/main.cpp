ca6fb767487f35d8ebab910ec52593a81c848a00
Manuel Schrauth <manuel.schrauth@physik.uni-wuerzburg.de>
2021-01-15 11:12:13 +0100

    fix registry: forward suffix pattern to constructor

--- src/main.cpp
+++ src/main.cpp
@@ -64,10 +64,10 @@ void Loop(const std::vector<Parameters>& params, Callable filter)
 template <class Hamiltonian, class Params, class Callable>
 void RegularLatticeLoop(RegistryDB& reg, const std::string outbasedir, const std::vector<Params>& hp, Callable filter)
 {
-	const auto name      = reg.Get<std::string>("mc", "General", "Hamiltonian" );
-	      auto nreplicas = reg.Get<std::vector<int>>("mc", name, "rep" );
-	const auto nL  	 = reg.Get<std::vector<int>>("mc", name, "L" );
-	const auto dim 	 = reg.Get<int>("mc", name, "dim" );
+	const auto name      = reg.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+	      auto nreplicas = reg.Get<std::vector<int>>("mc.ini", name, "rep" );
+	const auto nL  	 = reg.Get<std::vector<int>>("mc.ini", name, "L" );
+	const auto dim 	 = reg.Get<int>("mc.ini", name, "dim" );
 
     typedef decltype(finalize_parameter_pair(std::declval<MARQOV::Config>(), hp)) PPType;
     
@@ -115,12 +115,12 @@ void RegularLatticeLoop(RegistryDB& reg, const std::string outbasedir, const std
 
 std::string selectsim_startup(RegistryDB& registry)
 {
-	const auto ham        = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-	const auto dim 	  = registry.Get<int>("mc", ham, "dim" );
-	const auto nreplicas  = registry.Get<std::vector<int>>("mc", ham, "rep" );
-	const auto nreplicass = registry.Get<std::string>("mc", ham, "rep" );
-	const auto nL  	  = registry.Get<std::vector<int>>("mc", ham, "L" );
-	const auto nLs 	  = registry.Get<std::string>("mc", ham, "L" );
+	const auto ham        = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+	const auto dim 	  = registry.Get<int>("mc.ini", ham, "dim" );
+	const auto nreplicas  = registry.Get<std::vector<int>>("mc.ini", ham, "rep" );
+	const auto nreplicass = registry.Get<std::string>("mc.ini", ham, "rep" );
+	const auto nL  	  = registry.Get<std::vector<int>>("mc.ini", ham, "L" );
+	const auto nLs 	  = registry.Get<std::string>("mc.ini", ham, "L" );
 
 	cout << endl;
 	cout << "Hamiltonian: \t" << ham << endl;
@@ -145,8 +145,8 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 
 	if (startswith(ham, "Ising"))
 	{
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
 		auto parameters = cart_prod(beta, J);
 
 		write_logfile(registry, beta);
@@ -154,8 +154,8 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
 	else if (ham == "Heisenberg")
 	{
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
 		auto parameters = cart_prod(beta, J);
 
 		write_logfile(registry, beta);
@@ -163,9 +163,9 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
     else if (ham == "Phi4")
     {
-		auto beta   = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto lambda = registry.Get<std::vector<double> >("mc", ham, "lambda");
-		auto mass   = registry.Get<std::vector<double> >("mc", ham, "mass");
+		auto beta   = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto lambda = registry.Get<std::vector<double> >("mc.ini", ham, "lambda");
+		auto mass   = registry.Get<std::vector<double> >("mc.ini", ham, "mass");
 
 		// we need "beta" as an explicit parameter in the Hamiltonian
 		// this requires some gymnastics ...
@@ -178,9 +178,9 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
     }
     else if (ham == "BlumeCapel")
     {
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
-		auto D    = registry.Get<std::vector<double> >("mc", ham, "D");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
+		auto D    = registry.Get<std::vector<double> >("mc.ini", ham, "D");
 		auto parameters = cart_prod(beta, J, D);
 
 		write_logfile(registry, beta);
@@ -188,9 +188,9 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
     }
     else if (startswith(ham, "AshkinTeller"))
     {
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
-		auto K    = registry.Get<std::vector<double> >("mc", ham, "K");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
+		auto K    = registry.Get<std::vector<double> >("mc.ini", ham, "K");
 		auto parameters = cart_prod(beta, J, K);
 
 		write_logfile(registry, beta);
@@ -198,9 +198,9 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
 	else if (ham == "XXZAntiferro")
 	{
-		auto beta     = registry.Get<std::vector<double>>("mc", ham, "beta");
-		auto extfield = registry.Get<std::vector<double>>("mc", ham, "extfield");
-		auto aniso    = registry.Get<std::vector<double>>("mc", ham, "aniso");
+		auto beta     = registry.Get<std::vector<double>>("mc.ini", ham, "beta");
+		auto extfield = registry.Get<std::vector<double>>("mc.ini", ham, "extfield");
+		auto aniso    = registry.Get<std::vector<double>>("mc.ini", ham, "aniso");
 		auto parameters = cart_prod(beta, aniso, extfield);
 
 		write_logfile(registry, beta);
@@ -208,10 +208,10 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
 	else if (ham == "XXZAntiferroSingleAniso")
 	{
-		auto beta        = registry.Get<std::vector<double>>("mc", ham, "beta");
-		auto extfield    = registry.Get<std::vector<double>>("mc", ham, "extfield");
-		auto aniso       = registry.Get<std::vector<double>>("mc", ham, "aniso");
-		auto singleaniso = registry.Get<std::vector<double>>("mc", ham, "singleaniso");
+		auto beta        = registry.Get<std::vector<double>>("mc.ini", ham, "beta");
+		auto extfield    = registry.Get<std::vector<double>>("mc.ini", ham, "extfield");
+		auto aniso       = registry.Get<std::vector<double>>("mc.ini", ham, "aniso");
+		auto singleaniso = registry.Get<std::vector<double>>("mc.ini", ham, "singleaniso");
 		auto parameters = cart_prod(beta, extfield, aniso, singleaniso);
 
 		write_logfile(registry, extfield);
@@ -219,15 +219,15 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
 	else if (startswith(ham, "Bimodal-Ising-EdwardsAnderson"))
 	{
-		const auto ham        = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-		const auto dim 	  = registry.Get<int>("mc", ham, "dim" );
-		      auto nreplicas  = registry.Get<std::vector<int>>("mc", ham, "rep" );
-		const auto nL  	  = registry.Get<std::vector<int>>("mc", ham, "L" );
+		const auto ham        = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+		const auto dim 	  = registry.Get<int>("mc.ini", ham, "dim" );
+		      auto nreplicas  = registry.Get<std::vector<int>>("mc.ini", ham, "rep" );
+		const auto nL  	  = registry.Get<std::vector<int>>("mc.ini", ham, "L" );
 
 		if (nreplicas.size() == 1) { for (int i=0; i<nL.size()-1; i++) nreplicas.push_back(nreplicas[0]); }
 
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
 
 		auto hp = cart_prod(beta, J);
 		write_logfile(registry, beta);
@@ -268,15 +268,15 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
 	else if (startswith(ham, "Gaussian-Ising-EdwardsAnderson"))
 	{
-		const auto ham        = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-		const auto dim 	  = registry.Get<int>("mc", ham, "dim" );
-		      auto nreplicas  = registry.Get<std::vector<int>>("mc", ham, "rep" );
-		const auto nL  	  = registry.Get<std::vector<int>>("mc", ham, "L" );
+		const auto ham        = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+		const auto dim 	  = registry.Get<int>("mc.ini", ham, "dim" );
+		      auto nreplicas  = registry.Get<std::vector<int>>("mc.ini", ham, "rep" );
+		const auto nL  	  = registry.Get<std::vector<int>>("mc.ini", ham, "L" );
 
 		if (nreplicas.size() == 1) { for (int i=0; i<nL.size()-1; i++) nreplicas.push_back(nreplicas[0]); }
 
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
 
 		auto hp = cart_prod(beta, J);
 		write_logfile(registry, beta);
@@ -318,16 +318,16 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	else if (ham == "SSH")
 	{
 
-		auto beta   = registry.Get<std::vector<double> >("mc", ham, "betaMC");
-		auto betaQM = registry.Get<double>("mc", ham, "betaQM");
-		auto m      = registry.Get<std::vector<double> >("mc", ham, "m");
-		auto k      = registry.Get<std::vector<double> >("mc", ham, "k");
+		auto beta   = registry.Get<std::vector<double> >("mc.ini", ham, "betaMC");
+		auto betaQM = registry.Get<double>("mc.ini", ham, "betaQM");
+		auto m      = registry.Get<std::vector<double> >("mc.ini", ham, "m");
+		auto k      = registry.Get<std::vector<double> >("mc.ini", ham, "k");
 
-		const auto name      = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-		      auto nreplicas = registry.Get<std::vector<int>>("mc", name, "rep" );
-		const auto nL 	      = registry.Get<std::vector<int>>("mc", name, "L" );
-		const auto nLtime  	 = registry.Get<std::vector<int>>("mc", name, "Ltime" );
-		const auto dim 	 = registry.Get<int>("mc", name, "dim" );
+		const auto name      = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+		      auto nreplicas = registry.Get<std::vector<int>>("mc.ini", name, "rep" );
+		const auto nL 	      = registry.Get<std::vector<int>>("mc.ini", name, "L" );
+		const auto nLtime  	 = registry.Get<std::vector<int>>("mc.ini", name, "Ltime" );
+		const auto dim 	 = registry.Get<int>("mc.ini", name, "dim" );
 
 
 
@@ -377,15 +377,15 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	*/
 	else if (ham == "IsingCC")
 	{
-		const auto ham        = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-		const auto dim 	  = registry.Get<int>("mc", ham, "dim" );
-		      auto nreplicas  = registry.Get<std::vector<int>>("mc", ham, "rep" );
-		const auto nL  	  = registry.Get<std::vector<int>>("mc", ham, "L" );
+		const auto ham        = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+		const auto dim 	  = registry.Get<int>("mc.ini", ham, "dim" );
+		      auto nreplicas  = registry.Get<std::vector<int>>("mc.ini", ham, "rep" );
+		const auto nL  	  = registry.Get<std::vector<int>>("mc.ini", ham, "L" );
 
 		if (nreplicas.size() == 1) { for (int i=0; i<nL.size()-1; i++) nreplicas.push_back(nreplicas[0]); }
 
-		auto beta = registry.Get<std::vector<double> >("mc", "IsingCC", "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", "IsingCC", "J");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", "IsingCC", "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", "IsingCC", "J");
 
 		auto hp = cart_prod(beta, J);
 		write_logfile(registry, beta);
@@ -440,7 +440,7 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 		makeDir(outpath);
 
 		// Hamiltonian parameters
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
 		std::vector<double> myj = {-1.0};
 		auto hp = cart_prod(beta, myj);
 
@@ -466,16 +466,16 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 	}
     else if (ham == "BlumeCapelBipartite")
     {
-		auto beta = registry.Get<std::vector<double> >("mc", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc", ham, "J");
-		auto DA   = registry.Get<std::vector<double> >("mc", ham, "DA");
-		auto DB   = registry.Get<std::vector<double> >("mc", ham, "DB");
+		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
+		auto DA   = registry.Get<std::vector<double> >("mc.ini", ham, "DA");
+		auto DB   = registry.Get<std::vector<double> >("mc.ini", ham, "DB");
 		auto parameters = cart_prod(beta, J, DA, DB);
 
-		const auto name      = registry.Get<std::string>("mc", "General", "Hamiltonian" );
-		      auto nreplicas = registry.Get<std::vector<int>>("mc", name, "rep" );
-		const auto nL 	      = registry.Get<std::vector<int>>("mc", name, "L" );
-		const auto dim 	 = registry.Get<int>("mc", name, "dim" );
+		const auto name      = registry.Get<std::string>("mc.ini", "General", "Hamiltonian" );
+		      auto nreplicas = registry.Get<std::vector<int>>("mc.ini", name, "rep" );
+		const auto nL 	      = registry.Get<std::vector<int>>("mc.ini", name, "L" );
+		const auto dim 	 = registry.Get<int>("mc.ini", name, "dim" );
 
 		write_logfile(registry, beta);
         
@@ -544,11 +544,11 @@ int main()
 {
 
 	// read config files
-	RegistryDB registry("../src/config");
+	RegistryDB registry("../src/config", "ini");
 
 	// remove old output and prepare new one
-	std::string outbasedir = registry.Get<std::string>("mc", "IO", "outdir" );
-	std::string logbasedir = registry.Get<std::string>("mc", "IO", "logdir" );
+	std::string outbasedir = registry.Get<std::string>("mc.ini", "IO", "outdir" );
+	std::string logbasedir = registry.Get<std::string>("mc.ini", "IO", "logdir" );
 
 	//FIXME: NEVER DELETE USER DATA
 	std::string command;
