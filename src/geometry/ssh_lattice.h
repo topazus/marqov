04b51cf94a00db2ea16adf2593d67c6f2c2419a8
Manuel Schrauth <manuel.schrauth@physik.uni-wuerzburg.de>
2021-01-20 00:05:58 +0100

    unify 1D and 2D SSH lattice

--- src/geometry/ssh_lattice.h
+++ src/geometry/ssh_lattice.h
@@ -1,6 +1,9 @@
 #ifndef SSHLATTE_H
 #define SSHLATTE_H
 
+// covers the 1+1 dimensional and the 2+1 dimensional case
+// note that the dimension is set via the proceessor statement SSH_2D
+
 class SSHLattice
 {
 	public:
@@ -13,22 +16,54 @@ class SSHLattice
 		
 		SSHLattice(int l, int ltau, int d) : len(l), lentime(ltau), dim(d) // dim is the spatial dimension!
 		{
-		    vol = dim * pow(len, dim); // spatial volume (number of bonds in a time slice)
-		    nsites = vol * lentime; // total volume (number of sites)
+			// dim from config file will be ignored (improve me!)
+
+			#ifdef SSH_2D
+				dim = 2;
+			#else
+				dim = 1;
+			#endif
+
+			vol = dim * pow(len, dim); // spatial volume (number of bonds in a time slice)
+			nsites = vol * lentime; // total volume (number of sites)
 		}
+
+
+		//    illustration of the bond arrangement in 2+1 dimensions
+		//    horizontal and vertical bonds are arranged in an alternating fashion
+		//    shown are the time slices t=0 and t=1 for a lattice of size L=3
+		//    X's denote the coordinate grid, numbers represent the bonds
+		// 
+		//    o       o       o                o       o       o
+		//    |       |       |                |       |       |
+		//    15      16      17               33      34      35
+		//    |       |       |                |       |       |
+		//    X---12--X---13--X---14--o        X---30--X---31--X---32--o
+		//    |       |       |                |       |       |
+		//    9       10      11               27      28      29
+		//    |       |       |                |       |       |
+		//    X---6---X---7---X---8---o        X---24--X---25--X---26--o
+		//    |       |       |                |       |       |
+		//    3       4       5                21      22      23
+		//    |       |       |                |       |       |
+		//    X---0---X---1---X---2---o        X---18--X---19--X---20--o
+
+
 		
 		value_type getnbrs(int a, int i) const 
 		{
 			value_type retval;
-
-			const int k = i / vol;
-			const int offset = i % vol;
+  			int tslice = i / vol; // time slice
+  			int offset = i % vol; // position (offset) in time slice
 
 			switch(a) 
 			{
+				// returns the (two) neighbours along the time direction
 				case 0:
-					retval = {((k-1+lentime)%lentime)*vol+offset, ((k+1)%lentime)*vol+offset};
+  					retval = {((tslice-1+lentime)%lentime)*vol+offset, ((tslice+1)%lentime)*vol+offset};
 					break;
+
+				// returns the entire lattice
 				case 1:
 					retval.reserve(this->nsites);
 					for(int j = 0; j < i; ++j)
@@ -42,44 +77,47 @@ class SSHLattice
 		}
 
 
-// spatial ordering for a 2+1 dimensional setting (L=3, Ltime>3)
-
-//    | | |    | | |   | | |
-//    | | .    | | .   | | . 
-//    | . 24   | . 25  | . 26
-//    . 21.    . 22.   . 23.
-//    18. |    19. |   20. |
-//    . | |    . | |   . | |
-//    | | |    | | |   | | |
-//    | | .    | | .   | | .
-//    | . 15   | . 16  | . 17
-//    . 12.    . 13    . 14.
-//    9 . |    10. |   11 .|
-//    . | |    . | |   . | |
-//    | | |    | | |   | | |
-//    | | .    | | .   | | .
-//    | . 6    | . 7   | . 8
-//    . 3      . 4     . 5
-//    0        1       2
-
-// e.g. getnbrs(a=0, 10): 1, 19
-
-
-		
-		std::vector<double> getcrds(int k) const 
+		std::vector<double> getcrds(int i) const 
 		{
-		 	// transform one-dimensional index to n-d coordinates
-			std::vector<int> indices = IndexOfRect(k, dim, len, lentime);
-		 	std::vector<double> retval(dim,0);
+			// returns bond coordinates, which is five numbers in 2+1 dimension
+   			// (xstart, xend, ystart, yend, time)
+			// and only three numbers in 1+1 dimension (xstart, xend, time)
+   			
+   			const int tslice = i / vol;  // time slice
+   			const int offset = i % vol;  // position (offset) in time slice
 
-		 	for (int i=0; i<retval.size(); i++) 
-		 	{ 
-		 		retval[i] = double(indices[i]); ///len; 
-//		 		retval[i] += 0.5/len;
-		 	}
-		
-			return retval;
+			#ifdef SSH_2D
+   			
+   				const int yslice = offset / len;  // y-row
+   				const int xpos   = offset % len;  // position in y-row
+   				
+   				// since horizontal and vertical bonds are arranged in an alternating fashion (designers choice!)
+   				// we require to extract the actual y coordinate
+   				int yreal = yslice/2;
+
+				if (yslice%2 == 0)
+				{
+					const int left = xpos;
+					const int right = (xpos+1)%len;  // account for p.b.c in x
+					return {left, right, yreal, yreal, tslice};
+				}
+				else
+				{
+					const int lower = yreal;
+					const int upper = (yreal+1)%len; // account for p.b.c in y
+					return {xpos, xpos, lower, upper, tslice};
+				}
+
+			#else
+				const int left = offset;
+				const int right = (offset+1)%len;
+				return {left, right, tslice};
+			#endif
 		 }
+
+
+
+
 		
 		 value_type getbnds(int a, int i) const 
 		 {
