75ec6432d73260fc85f8e931056abc5fb2a3bff9
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-05-04 18:02:11 +0200

    Add license header and class docs

--- src/geometry/points.h
+++ src/geometry/points.h
@@ -1,3 +1,22 @@
+/* This file is part of MARQOV:
+ * A modern framework for classical spin models on general topologies
+ * Copyright (C) 2020-2021, The MARQOV Project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef POINTS_H
 #define POINTS_H
 
@@ -6,8 +25,9 @@
 
 // implement simple class for "site" which only carries the coordinate
 
-
-// Point cloud base class
+/**
+ * Point cloud base class.
+ */
 class PointCloud
 {
 	protected:
@@ -28,8 +48,9 @@ class PointCloud
 		std::size_t size() const {return npoints; }
 };
 
-
-// random Poissonian point cloud
+/**
+ * Random Poissonian point cloud
+ */
 class Poissonian : public PointCloud
 {
 	public:
@@ -51,9 +72,10 @@ class Poissonian : public PointCloud
 		}
 };
 
-
-// regular square point cloud
-// improve me: calculate coordinates on demand
+/**
+ * regular square point cloud
+ * FIXME: calculate coordinates on demand
+ */
 class RegularSquare : public PointCloud
 {
 	public:
