14abfa28617b4d1d3fcc6b719aed1bb493d4ba3a
Manuel Schrauth <manuel.schrauth@physik.uni-wuerzburg.de>
2021-05-11 23:19:25 +0200

    remove Ising-CCref; move RegularLatticeLoop to util.h

--- src/main.cpp
+++ src/main.cpp
@@ -40,6 +40,7 @@ using std::ofstream;
 #include "svmath.h"
 #include "filters.h"
 #include "marqovscheduler.h"
+#include "util.h"
 
 // Geometry
 #include "geometry/regular_lattice.h"
@@ -64,78 +65,6 @@ using std::ofstream;
 using namespace MARQOV;
 
 
-template <class Hamiltonian, class Params, class Callable>
-void RegularLatticeLoop(RegistryDB& reg, const std::string outbasedir, const std::vector<Params>& hp, Callable filter)
-{
-	
-	// Typedefs
-	typedef decltype(finalize_parameter_pair(std::declval<MARQOV::Config>(), hp)) ParameterPairType;
-	typedef typename ParameterPairType::value_type ParameterType;
-	typedef typename GetSchedulerType<Hamiltonian, RegularHypercubic, ParameterType>::MarqovScheduler SchedulerType;
-
-
-	// Parameters
-	const auto name = reg.Get<std::string>("mc.ini", "General", "Hamiltonian" );
-	auto nreplicas  = reg.Get<std::vector<int>>("mc.ini", name, "rep" );
-	const auto nL   = reg.Get<std::vector<int>>("mc.ini", name, "L" );
-	const auto dim  = reg.Get<int>("mc.ini", name, "dim" );
-
-
-	// Number of threads
-	int nthreads = 0;
-	try 
-	{
-		nthreads = reg.template Get<int>("mc.ini", "General", "threads_per_node" );
-	}
-	catch (const Registry_Key_not_found_Exception&) 
-	{
-		std::cout<<"threads_per_node not set -> automatic"<<std::endl;
-	}
-
-	// Prepare Geometry
-	if (nreplicas.size() == 1) { for (decltype(nL.size()) i=0; i<nL.size()-1; i++) nreplicas.push_back(nreplicas[0]); }
-	std::vector<RegularHypercubic> latts;
-	for (std::size_t j=0; j<nL.size(); j++) latts.emplace_back(nL[j], dim);
-    
-
-	// Init Scheduler
-	SchedulerType sched(1, nthreads);
-    
-
-
-	for (std::size_t j=0; j<nL.size(); j++)
-	{
-		// prepare
-		int L = nL[j];
-		cout << endl << "L = " << L << endl << endl;
-
-		std::string outpath = outbasedir+"/"+std::to_string(L)+"/";
-
-		MARQOV::Config mp(outpath);
-		mp.setnsweeps(10);
-		mp.setncluster(int(L/2));
-		mp.setwarmupsteps(200);
-		mp.setgameloopsteps(500);
-		
-		makeDir(mp.outpath);
-		
-		auto params = finalize_parameter_pair(mp, hp);
-		auto rparams = replicator_pair(params, nreplicas[j]);
-		
-		// set up and execute        
-		RegularHypercubic& latt = latts[j];
-
-		auto f = [&filter, &latt](auto p){return filter(latt, p);}; //partially apply filter
-		for(auto p : rparams) sched.createSimfromParameter(p, f);
-	}
-	sched.start();
-}
-
-
-
-
-
-// ---------------------------------------
 
 std::string selectsim_startup(RegistryDB& registry)
 {
@@ -421,47 +350,6 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 
 
 
-
-	else if (ham == "IsingCC-ref")
-	{
-		// construct irregular lattice and pass it to MARQOV as a reference
-
-		const int L = 32;
-		const int dim = 2;
-		ConstantCoordinationLattice<Poissonian> ccl(L, dim);
-
-		// prepare output
-		std::string outpath = outbasedir+"/"+std::to_string(L)+"/";
-		makeDir(outpath);
-
-		// Hamiltonian parameters
-		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
-		std::vector<double> myj = {-1.0};
-		auto hp = cart_prod(beta, myj);
-
-		// Monte Carlo parameters
-		MARQOV::Config mp(outpath);
-		mp.setrepid(1);
-		mp.setnsweeps(L);
-		mp.setncluster(10);
-
-		auto params = finalize_parameter_pair(mp, hp);
-		// partially apply filter
-		auto f = [&ccl](auto p){return defaultfilter(ccl, p);};
-
-		// typedefs
-		typedef typename decltype(params)::value_type ParameterPairType;
-		typedef Ising<int> Hamiltonian;
-		typedef ConstantCoordinationLattice<Poissonian> Lattice;
-		typedef typename GetSchedulerType<Hamiltonian, Lattice, ParameterPairType>::MarqovScheduler SchedulerType;
-		
-		// init and feed scheduler
-		SchedulerType sched(1);
-		for (auto p: params) sched.createSimfromParameter(p, f);
-
-		// run!
-		sched.start();
-	}
 
 
 
