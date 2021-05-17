fa09f480c352032de1c2f8c79815157bbcbec7e9
Manuel <manuel.schrauth@physik.uni-wuerzburg.de>
2021-05-17 23:00:31 +0200

    something that compiles but throws runtime errors ...

--- src/wolff.h
+++ src/wolff.h
@@ -31,6 +31,45 @@ namespace MARQOV
      * @tparam Hamiltonian The Hamiltonian that the Wolff algo will use.
      * @tparam Lattice The Lattice, that the Wolff algo should use.
      */
+
+template <class Hamiltonian, class StateVector, class NeighbourType, class = void>
+struct has_wolff_embedding : std::false_type{};
+
+template <class Hamiltonian, class StateVector, class NeighbourType>
+struct has_wolff_embedding<Hamiltonian, StateVector, NeighbourType,
+	MARQOV::detail::type_sink_t< decltype( std::declval<Hamiltonian>().template wolff_embedding<StateVector,NeighbourType>(
+		std::declval<StateVector>(), std::declval<NeighbourType>()) )
+	>> : std::true_type{};
+
+
+template <class Hamiltonian, class StateVector, class NeighbourType>
+auto wolff_embedding_helper(Hamiltonian ham, StateVector sv, NeighbourType nbrs, std::true_type)
+{
+	cout << "true";
+	return 1;
+//	return ham.wolff_embedding(sv, nbrs);
+}
+
+
+template <class Hamiltonian, class StateVector, class NeighbourType>
+auto wolff_embedding_helper(Hamiltonian ham, StateVector sv, NeighbourType nbrs, std::false_type)
+{
+	cout << "true";
+	return 0;
+}
+
+
+template <class Hamiltonian, class StateVector, class NeighbourType>
+auto wolff_embedding(Hamiltonian ham, StateVector sv, NeighbourType nbrs)
+{
+	return wolff_embedding_helper(ham, sv, nbrs, has_wolff_embedding<Hamiltonian, StateVector, NeighbourType>{});
+}
+
+
+
+
+
+
     template <class Hamiltonian, class Lattice>
     struct Wolff
     {
@@ -46,15 +85,13 @@ namespace MARQOV
         // prepare stack
         std::vector<int> cstack(grid.size(), 0);
 
-        const double global_coupling = ham.interactions[a]->J;
-        
         // add initial site and flip it
         int q = 0;
         cstack[q] = rsite;
 
-		ham.wolff_embedd_init(); // the random direction/color whatever is stored in the Hamiltonian! this function resets it
+//		ham.wolff_embedd_init(); // the random direction/color whatever is stored in the Hamiltonian! this function resets it
         
-        ham.wolff_flip(statespace[rsite], rdir);
+        ham.wolff_flip(statespace[rsite], rdir); // remove rdir, it is implicit now!
         int clustersize = 1;
         
         // loop over stack as long as non-empty
@@ -67,17 +104,22 @@ namespace MARQOV
             
             // get its neighbours
             int a = 0; // to be replaced by loop over Nalpha
+        	const double global_coupling = ham.interactions[a]->J;
             const auto nbrs = grid.nbrs(a, currentidx);
-            const auto bnds = grid.bnds(a, currentidx); // important: specify what to do if function is not there
+//            const auto bnds = grid.bnds(a, currentidx); // important: specify what to do if function is not there
 
-            const auto cpl1 = ham.wolff_coupling(currentsv, nbrs); 
+            const auto cpl1 = wolff_embedding<Hamiltonian,StateVector,decltype(std::vector<int>())>(ham, currentsv, nbrs); 
+
+			cout << "cpl1" << endl;
+
 			// rdir is implicitely stored in the Hamiltonian! (actually this function should be named "wolff_embedding")
-            const auto cpl2 = ham.wolff_scalarize(grid.nbrs(a, currentidx)); 
+//            const auto cpl2 = ham.wolff_scalarize(currentsv, bnds); 
 			// specifiy what to do with the bond strength; 
 			// in practice has to communicate with wolff_coupling via rdir which is stored in the lattice
 			// todo: if function is not there, return array of 1's
 
             // loop over neighbours
+			/*
             for (std::size_t i = 0; i < nbrs.size(); ++i)
             {
                 // extract corresponding sv
@@ -98,6 +140,7 @@ namespace MARQOV
                     }
                 }
             }
+			*/
         }
         return clustersize;
     }
@@ -110,35 +153,6 @@ namespace MARQOV
     }
 };
 
-
-template <class Hamiltonian, class = void>
-struct has_wolff_embedding : std::false_type{};
-
-template <class Hamiltonian, class = void>
-struct has_wolff_embedding<Hamiltonian, MARQOV::detail::type_sink_<
-	decltype( ...
-	>> : std::true_type{};
-
-
-template <class Hamilonian, class BondType>
-auto wolff_embedding_helper(Hamiltonian& ham, BondType bond, std::true_type)
-{
-	return ham.wolff_embedding(bond);
-}
-
-
-template <class Hamilonian, class BondType>
-auto wolff_embedding_helper(Hamiltonian& ham, BondType bond, std::false)
-{
-	return bond[0];
-}
-
-
-template <class Hamilonian, class BondType>
-auto wolff_embedding(Hamiltonian& ham, BondType bond)
-{
-	return wolff_embedding_helper(ham, bond, has_wolff_embedding<Hamiltonian>{});
-}
 
 
 #endif
