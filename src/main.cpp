fcc00ca5ee4fcf6d2a8cf8cbb606a1cb894ae1fe
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2021-05-21 19:54:46 +0200

    Fix a PGI warning and make Edwards Anderson thread aware.

--- src/main.cpp
+++ src/main.cpp
@@ -253,7 +253,7 @@ void selectsim(RegistryDB& registry, std::string outbasedir, std::string logbase
 		typedef typename ParameterTripleType::value_type ParameterType;
 		typedef typename GetSchedulerType<Hamiltonian, Lattice, ParameterType>::MarqovScheduler SchedulerType;
 
-		SchedulerType sched(1);
+		SchedulerType sched(1, nthreads);
 
 		// Lattice size loop
 		for (std::size_t j=0; j<nL.size(); j++)
