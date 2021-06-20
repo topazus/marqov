7afaa583a9899095cceca79799051b2738668c14
Manuel Schrauth <manuel.schrauth@physik.uni-wuerzburg.de>
2021-06-20 23:26:27 +0200

    registry adjustments

--- src/main.cpp
+++ src/main.cpp
@@ -161,9 +161,9 @@ void selectsim()
 
 	else if (ham == "BlumeCapel")
 	{
-		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
-		auto D    = registry.Get<std::vector<double> >("mc.ini", ham, "D");
+		auto beta = registry.Get<std::vector<double> >(ham+".ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >(ham+".ini", ham, "J");
+		auto D    = registry.Get<std::vector<double> >(ham+".ini", ham, "D");
 		auto parameters = cart_prod(beta, J, D);
 		
 		RegularLatticeLoop<BlumeCapel<int>>(registry, outbasedir, parameters, defaultfilter);
@@ -173,10 +173,10 @@ void selectsim()
 
 	else if (ham == "BlumeEmeryGriffiths")
 	{
-		auto beta = registry.Get<std::vector<double> >("mc.ini", ham, "beta");
-		auto J    = registry.Get<std::vector<double> >("mc.ini", ham, "J");
-		auto D    = registry.Get<std::vector<double> >("mc.ini", ham, "D");
-		auto K    = registry.Get<std::vector<double> >("mc.ini", ham, "K");
+		auto beta = registry.Get<std::vector<double> >(ham+".ini", ham, "beta");
+		auto J    = registry.Get<std::vector<double> >(ham+".ini", ham, "J");
+		auto D    = registry.Get<std::vector<double> >(ham+".ini", ham, "D");
+		auto K    = registry.Get<std::vector<double> >(ham+".ini", ham, "K");
 		auto parameters = cart_prod(beta, J, D, K);
 		
 		RegularLatticeLoop<BlumeEmeryGriffiths<int>>(registry, outbasedir, parameters, defaultfilter);
@@ -186,9 +186,9 @@ void selectsim()
 
 	else if (ham == "XXZAntiferro")
 	{
-		auto beta     = registry.Get<std::vector<double>>("mc.ini", ham, "beta");
-		auto extfield = registry.Get<std::vector<double>>("mc.ini", ham, "extfield");
-		auto aniso    = registry.Get<std::vector<double>>("mc.ini", ham, "aniso");
+		auto beta     = registry.Get<std::vector<double>>(ham+".ini", ham, "beta");
+		auto extfield = registry.Get<std::vector<double>>(ham+".ini", ham, "extfield");
+		auto aniso    = registry.Get<std::vector<double>>(ham+".ini", ham, "aniso");
 		auto parameters = cart_prod(beta, aniso, extfield);
 		
 		RegularLatticeLoop<XXZAntiferro<double>>(registry, outbasedir, parameters, defaultfilter);
@@ -198,10 +198,10 @@ void selectsim()
 
 	else if (ham == "XXZAntiferroSingleAniso")
 	{
-		auto beta        = registry.Get<std::vector<double>>("mc.ini", ham, "beta");
-		auto extfield    = registry.Get<std::vector<double>>("mc.ini", ham, "extfield");
-		auto aniso       = registry.Get<std::vector<double>>("mc.ini", ham, "aniso");
-		auto singleaniso = registry.Get<std::vector<double>>("mc.ini", ham, "singleaniso");
+		auto beta        = registry.Get<std::vector<double>>(ham+".ini", ham, "beta");
+		auto extfield    = registry.Get<std::vector<double>>(ham+".ini", ham, "extfield");
+		auto aniso       = registry.Get<std::vector<double>>(ham+".ini", ham, "aniso");
+		auto singleaniso = registry.Get<std::vector<double>>(ham+".ini", ham, "singleaniso");
 		auto parameters = cart_prod(beta, extfield, aniso, singleaniso);
 
 		RegularLatticeLoop<XXZAntiferroSingleAniso<double>>(registry, outbasedir, parameters, xxzfilter);
