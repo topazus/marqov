1dae088a6cc0537df8a9e18a2f3bb3597603e36d
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-07-07 23:28:20 +0200

    firs try at proper paths for pymarqov

--- createfulldocs.sh
+++ createfulldocs.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 # this script creates the full documentation, including python
-# For this to work, gitlab wil lask for credentials.
-git clone https://git.physik.uni-wuerzburg.de/marqov/jupyter.git
-rm jupyter/pymarqov/doc/conf.py
+# For this to work, gitlab will ask for credentials.
+git clone https://git.physik.uni-wuerzburg.de/marqov/pymarqov.git
+rm pymarqov/doc/conf.py
 cd doc
-cp -rn ../jupyter/pymarqov/doc python
-echo "sys.path.insert(0, os.path.abspath('../jupyter/pymarqov/'))" >> conf.py
+cp -rn ../pymarqov/doc python
+echo "sys.path.insert(0, os.path.abspath('../pymarqov/'))" >> conf.py
 doxygen Doxyfile-marqov-breathe.cfg
 make html
