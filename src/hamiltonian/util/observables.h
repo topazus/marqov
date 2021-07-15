c68896067636f76b0dc54ae7a6a8d52e0b09a61e
Manuel <manuel.schrauth@physik.uni-wuerzburg.de>
2021-07-15 21:59:38 +0200

    fix energy

--- src/hamiltonian/util/observables.h
+++ src/hamiltonian/util/observables.h
@@ -179,7 +179,7 @@ class InteractionEnergy
 
 				ene += ham.interactions[a]->J * enepart;
 			}
-			return ene/double(N);
+			return 0.5*ene/double(N); // account for double counting
 		}
 
 		template <class StateSpace, class Grid>
