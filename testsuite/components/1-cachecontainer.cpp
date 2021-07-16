b3210f7e0ec2defe7047e6c7a2923a0f68aff1da
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-07-16 21:50:20 +0200

    bugfix test

--- testsuite/components/1-cachecontainer.cpp
+++ testsuite/components/1-cachecontainer.cpp
@@ -9,8 +9,11 @@ int main()
     std::string filename("test.h5");
     auto flag = H5F_ACC_TRUNC;
     H5::H5File retval(filename, flag);
-    CacheContainer<double> testcont(retval, "testobs", "a test description");
-    CacheContainer<int> testint(retval, "testint", "a test int description");
+    H5::Group obs1(file.createGroup("obs1"));
+    H5::Group obs2(file.createGroup("obs1"));
+
+    CacheContainer<double> testcont(obs1, "testobs", "a test description");
+    CacheContainer<int> testint(obs2, "testint", "a test int description");
     for (uint i = 0; i < 2000000; ++i)
     {
         testcont<<42.0;
