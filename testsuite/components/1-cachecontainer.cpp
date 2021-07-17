721e16ecd603cd84354b001b5f8381ff914c672a
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-07-17 17:58:53 +0200

    bugfix to bugfix to bugfix

--- testsuite/components/1-cachecontainer.cpp
+++ testsuite/components/1-cachecontainer.cpp
@@ -10,7 +10,7 @@ int main()
     auto flag = H5F_ACC_TRUNC;
     H5::H5File file(filename, flag);
     H5::Group obs1(file.createGroup("obs1"));
-    H5::Group obs2(file.createGroup("obs1"));
+    H5::Group obs2(file.createGroup("obs2"));
 
     CacheContainer<double> testcont(obs1, "testobs", "a test description");
     CacheContainer<int> testint(obs2, "testint", "a test int description");
