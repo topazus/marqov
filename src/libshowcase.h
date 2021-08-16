4586c0456088d2b5c3b8e3cd1c4770e8a9686c94
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-08-16 18:09:44 +0200

    fix compilation

--- src/libshowcase.h
+++ src/libshowcase.h
@@ -2,8 +2,8 @@
 int pyIsing(std::string path, int dim, int len, double beta, double J);
 int pyHeisenberg(std::string path, int dim, int len, double beta, double J);
 int pyPhi4(std::string path, int dim, int len, double beta, double lambda, double mass);
-int pyAshkinTeller(std::string path, int dim, int len, double beta, double J, double K)
-int pyBlumeCapel(std::string path, int dim, int len, double beta, double J, double D)
-int pyBEG(std::string path, int dim, int len, double beta, double J, double D, double K)
-int pyPotts(int q, std::string path, int dim, int len, double beta, double J)
+int pyAshkinTeller(std::string path, int dim, int len, double beta, double J, double K);
+int pyBlumeCapel(std::string path, int dim, int len, double beta, double J, double D);
+int pyBEG(std::string path, int dim, int len, double beta, double J, double D, double K);
+int pyPotts(int q, std::string path, int dim, int len, double beta, double J);
 
