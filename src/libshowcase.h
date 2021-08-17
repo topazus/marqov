2b907f7b7ec643f8530d3a17cee1a77a6edaf756
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-08-17 18:01:02 +0200

    function prototype

--- src/libshowcase.h
+++ src/libshowcase.h
@@ -1,7 +1,10 @@
 #include <string>
 int pyIsing(std::string path, int dim, int len, double beta, double J);
 int pyIsingGraph(std::string outpath, std::string graphpath, double beta, double J);
+
 int pyHeisenberg(std::string path, int dim, int len, double beta, double J);
+int pyHeisenbergGraph(std::string outpath, std::string graphpath, double beta, double J)
+
 int pyPhi4(std::string path, int dim, int len, double beta, double lambda, double mass);
 int pyAshkinTeller(std::string path, int dim, int len, double beta, double J, double K);
 int pyBlumeCapel(std::string path, int dim, int len, double beta, double J, double D);
