d5082b7d2e0d4957dcc5009e58a86d4ddcec1f1f
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-08-17 17:40:35 +0200

    semicolon

--- src/libshowcase.h
+++ src/libshowcase.h
@@ -1,6 +1,6 @@
 #include <string>
 int pyIsing(std::string path, int dim, int len, double beta, double J);
-int pyIsingGraph(std::string outpath, std::string graphpath, double beta, double J)
+int pyIsingGraph(std::string outpath, std::string graphpath, double beta, double J);
 int pyHeisenberg(std::string path, int dim, int len, double beta, double J);
 int pyPhi4(std::string path, int dim, int len, double beta, double lambda, double mass);
 int pyAshkinTeller(std::string path, int dim, int len, double beta, double J, double K);
