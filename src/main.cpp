4a7f1015096709ce5d161f24d8948f1e91aab31b
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2021-08-20 22:30:08 +0200

    include registry into main

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ class MySimpleHeisenberg
 		std::array<MyHeisenberg_interaction*, 1> interactions = {new MyHeisenberg_interaction(J)};
         
 		// Constructor
-		MySimpleHeisenberg(double J) : J(J), name("MySimpleHeisenberg"), obs_e(*this){}
+		MySimpleHeisenberg(double J) : J(J), name("Heisenberg"), obs_e(*this){}
 		~MySimpleHeisenberg() {delete interactions[0];}
 
 		// Observables
@@ -59,11 +59,11 @@ using namespace MARQOV;
 
 int main()
 {
-	// We utilize a small helper library of ours, the registry which reads Windows .ini style files to read config files
-	RegistryDB registry;
+    // We utilize a small helper library of ours, the registry which reads Windows .ini style files to read config files
+    RegistryDB reg;
     try
     {
-        registry.init("../src/config", "ini");
+        reg.init("../src/config", "ini");
     }
     catch(Registry_Exception& re)
     {
@@ -76,7 +76,7 @@ int main()
         {
             std::ofstream select(filename);
             select<<"[General]"<<'\n'<<"Hamiltonian = Heisenberg"<<'\n'<<"[END]"<<std::endl;
-            registry.init("./config", "ini");
+            reg.init("./config", "ini");
         }
         else
         {
@@ -86,43 +86,45 @@ int main()
     }
     
     // With the registry available we can now start to read in parameters from config files. Note that these parameters get replicated in the final HDF5 containers.
-    // Initialize the lattice
-	int L = 8;
-	int dim = 2;
-    RegularHypercubic mylatt(L, dim);
-
-	// Set Monte Carlo parameters using MARQOV::Config
-	MARQOV::Config mp("./"); // output path 
-	mp.setnmetro(5); // number of Metropolis sweeps per EMCS
-	mp.setncluster(10); // number of Wolff updates per EMCS
-	mp.setwarmupsteps(500); // number of EMCS for warmup
-	mp.setgameloopsteps(3000); // number of EMCS for production
-
-	// Set the Hamiltonian parameters, J, and the inverse temperature beta
-    double beta = 0.66;
-    double J = 1;
-    auto hp = make_tuple(beta, J);
-
-    // Let's create some parameters tuples, a temperature scan for the scheduler to work on
-
-    // Prepare an array of parameter tuples
-    auto args = make_tuple(std::ref(mylatt), mp, hp);
-    vector<decltype(args)> v;
-
-	// Fill
-    for(int j = 0; j < 7; ++j)
-    {
-        hp = make_tuple(beta+j*0.1, J);
-        v.push_back(make_tuple(std::ref(mylatt), mp, hp));
-    }
-
-    // Set up the MARQOV schedular
-    typedef typename GetSchedulerType<MySimpleHeisenberg, RegularHypercubic, decltype(args)>::MarqovScheduler SchedulerType;
-    SchedulerType sched(1); //MARQOV makes use of all available threads by default.
     
-	// Feed parameters to the scheduler which creates the simulations
-    for(auto p : v)
-		sched.createSimfromParameter(p);
-	// Run
+    std::string name{"Heisenberg"};// We are a Heisenberg Model. Saves some typing.
+    std::string fn{name + ".ini"};
+    //let's get a couple of parameters from the registry
+    int nreplicas  = reg.Get<int>(fn, name, "rep" );
+    auto nclusteramp   = reg.Get<double>(fn, "MC", "nclusteramp");
+    auto nclusterexp   = reg.Get<int>(fn, "MC", "nclusterexp");
+    auto nmetro        = reg.Get<int>(fn, "MC", "nmetro");
+    auto warmupsteps   = reg.Get<int>(fn, "MC", "warmupsteps");
+    auto measuresteps  = reg.Get<int>(fn, "MC", "measuresteps");
+    int L = reg.Get<int>(fn, name, "L" );// a single lattice size
+    auto dim  = reg.Get<int>(fn, name, "dim" );
+    
+    //let's create a single lattice.
+    RegularHypercubic latt(L, dim);
+    
+    // Set the Hamiltonian parameters, J, and the inverse temperature beta
+    auto beta = reg.Get<std::vector<double> >(fn, "Heisenberg", "beta");//read in a set of inverse temperatures
+    auto J    = reg.Get<std::vector<double> >(fn, "Heisenberg", "J");//read in a set of coupling strengths
+    auto hp = cart_prod(beta, J);//Form the cartesian product of all betas and all Js, the hamiltonian parameters
+    
+    // prepare
+    std::string outpath = ".out/"+std::to_string(L)+"/";// by default we dump into the current directory.
+    
+    // Set Monte Carlo parameters using MARQOV::Config
+    MARQOV::Config mp(outpath);// output path 
+    mp.setnmetro(nmetro);// number of Metropolis sweeps per EMCS
+    mp.setncluster(int(nclusteramp*pow(L,nclusterexp)));// number of Wolff updates per EMCS
+    mp.setwarmupsteps(warmupsteps);// number of EMCS for warmup
+    mp.setgameloopsteps(measuresteps);// number of EMCS for production
+    
+    makeDir(mp.outpath);// A small utility function that helps us to create folders
+    
+    auto params = finalize_parameter(latt, mp, hp);//bundle the lattice, the marqov parameters and the hamiltonian parameters
+    auto rparams = replicator(params, nreplicas);//duplicate everything for the amount of replicas
+    
+    // Instantiate the scheduler which waits for new threads.
+    auto sched = makeScheduler<MySimpleHeisenberg, RegularHypercubic> (rparams[0]);// makeScheduler can figure out a lot from one set of parameters
+    
+    for(auto p : rparams) sched.createSimfromParameter(p);//submit parameter set to scheduler.
     sched.start();
 }
