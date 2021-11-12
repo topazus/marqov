e10aa7248d55e5d9d65618b601cb6d36f830aabb
Florian Goth <florian.goth@physik.uni-wuerzburg.de>
2021-11-12 15:11:05 +0100

    update heisenberg demo

--- demo/mysimpleheisenbergthreaded.cpp
+++ demo/mysimpleheisenbergthreaded.cpp
@@ -52,63 +52,58 @@ class MySimpleHeisenberg
 };
 
 template <class Lattice>
-    struct MARQOV::Wolff<MySimpleHeisenberg, Lattice>
+struct MARQOV::Wolff<MySimpleHeisenberg, Lattice>
+{
+    std::vector<int> cstack = std::vector<int>(4096/sizeof(int), 0);///< the size of the stack is meant to be preserved across different cluster processes.
+
+    template <class RNG, class StateSpace>
+    inline int move(const MySimpleHeisenberg& ham, const Lattice& grid, StateSpace& statespace, RNG& rng, double beta, int rsite)
     {
-//         std::vector<int> cstack = std::vector<int>(4096/sizeof(int), 0);
-        template <class RNG, class StateSpace>
-        /*static*/ inline int move(const MySimpleHeisenberg& ham, const Lattice& grid, StateSpace& statespace, RNG& rng, double beta, int rsite, int expectedclustersize)
+        typedef MySimpleHeisenberg Hamiltonian;
+        typedef typename Hamiltonian::StateVector StateVector;
+        constexpr static int SymD = Hamiltonian::SymD;
+        
+        int q = 0;
+        auto& seed = statespace[rsite];
+        cstack[q] = rsite;
+        
+        int rdir = rng.integer(SymD); // random Cartesian direction
+        seed[rdir] *= -1;
+        
+        int clustersize = 1;
+        int current = 0;
+        
+        // plain Heisenberg model has only one interaction term
+        const auto gcpl = ham.interactions[0]->J; 
+        
+        while (q>=0)
         {
-			typedef MySimpleHeisenberg Hamiltonian;
-            typedef typename Hamiltonian::StateVector StateVector;
-			constexpr static int SymD = Hamiltonian::SymD;
-
-             std::vector<int> cstack(
- grid.size()
-// //expectedclustersize
- , 0);
-            int q = 0;
-            auto& seed = statespace[rsite];
-            cstack[q] = rsite;
-
-			int rdir = rng.integer(SymD); // random Cartesian direction
-			seed[rdir] *= -1;
+            current = cstack[q];
+            q--;
             
-            int clustersize = 1;
-            int current = 0;
-				
-			// plain Heisenberg model has only one interaction term
-			const auto gcpl = ham.interactions[0]->J; 
-
-            while (q>=0)
+            const auto nbrs = grid.nbrs(0, current);
+            if(q + nbrs.size() > cstack.size()) 
+                cstack.resize(2*cstack.size());
+            for (std::size_t i = 0; i < nbrs.size(); ++i)
             {
-                current = cstack[q];
-                q--;
-
-				const auto nbrs = grid.nbrs(0, current);
-				for (std::size_t i = 0; i < nbrs.size(); ++i)
-				{
-					const auto currentnbr = nbrs[i];
-					StateVector& candidate = statespace[currentnbr];
-
-					const auto lcpl = statespace[current][rdir] * candidate[rdir];
-					const auto cpl  = gcpl*lcpl;
-                    
-                    if (wolff_update_accepted(cpl, beta, rng))
-                    {
-                        q++;
-                        clustersize++;
-// if (q < cstack.size())
-                        cstack[q] = currentnbr;
-// else
-// cstack.push_back(currentnbr);
-
-                        candidate[rdir] = -candidate[rdir];
-                    }
-				}
-			}
-			return clustersize;
+                const auto currentnbr = nbrs[i];
+                StateVector& candidate = statespace[currentnbr];
+                
+                const auto lcpl = statespace[current][rdir] * candidate[rdir];
+                const auto cpl  = gcpl*lcpl;
+                
+                if (wolff_update_accepted(cpl, beta, rng))
+                {
+                    q++;
+                    clustersize++;
+                    cstack[q] = currentnbr;
+                    candidate[rdir] = -candidate[rdir];
+                }
+            }
         }
-    };
+        return clustersize;
+    }
+};
 
 
 using namespace std;
@@ -117,19 +112,19 @@ using namespace MARQOV;
 int main()
 {
     // Initialize the lattice
-	int L = 32;
-	int dim = 3;
+	int L = 8;
+	int dim = 2;
     RegularHypercubic mylatt(L, dim);
 
 	// Set Monte Carlo parameters using MARQOV::Config
 	MARQOV::Config mp("./"); // output path 
-	mp.setnmetro(1); // number of Metropolis sweeps per EMCS
-	mp.setncluster(500); // number of Wolff updates per EMCS
+	mp.setnmetro(5); // number of Metropolis sweeps per EMCS
+	mp.setncluster(10); // number of Wolff updates per EMCS
 	mp.setwarmupsteps(500); // number of EMCS for warmup
-	mp.setgameloopsteps(500); // number of EMCS for production
+	mp.setgameloopsteps(3000); // number of EMCS for production
 
 	// Set the Hamiltonian parameters, J, and the inverse temperature beta
-    double beta = 0.7;
+    double beta = 0.66;
     double J = -1;
     auto hp = make_tuple(beta, J);
 
