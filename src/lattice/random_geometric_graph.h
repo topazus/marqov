4fc97c0035c4f5409881bd47b98c084307298305
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2022-01-26 13:21:26 +0100

    Add license

--- src/lattice/random_geometric_graph.h
+++ src/lattice/random_geometric_graph.h
@@ -1,6 +1,25 @@
+/* This file is part of MARQOV:
+ * A modern framework for classical spin models on general topologies
+ * Copyright (C) 2022, The MARQOV Project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef RGG_LATTICE_H
 #define RGG_LATTICE_H
 
+#include <vector>
 #include "util/points.h"	
 #include "util/distance.h"	
 #include <pcl/point_cloud.h>
@@ -267,7 +286,7 @@ class RandomGeometricGraph
 			return cloud.crds(i);
 		}
 
-		std::size_t size() const {return npoints;}
+		std::size_t size() const noexcept {return npoints;}
 };
 
 #endif
