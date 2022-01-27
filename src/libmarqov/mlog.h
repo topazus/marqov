b1aafdae36f23087ddcf2d3c1982d5393e716b5d
Florian Goth <fgoth@physik.uni-wuerzburg.de>
2022-01-27 16:39:02 +0100

    map flog into our own namespace

---
+++ src/libmarqov/mlog.h
@@ -0,0 +1,32 @@
+/* This file is part of MARQOV:
+ * A modern framework for classical spin models on general topologies
+ * Copyright (C) 2022, The MARQOV Project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef MLOG_H
+#define MLOG_H
+
+#include "flog/flog.h"
+
+#define MLOG_EXTLEVEL FLOG_EXTLEVEL
+
+#define MLOGDEBUGVERBOSE FLOGDEBUGVERBOSE
+#define MLOGDEBUG FLOGDEBUG
+#define MLOGRELEASEVERBOSE FLOGRELEASEVERBOSE
+#define MLOGVERBOSE FLOGVERBOSE
+#define MLOGRELEASE FLOGRELEASE
+
+#endif
